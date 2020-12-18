d062fb0c6452b3e6a27f5c575b7a72a05c7bfdc0
Hendrik <git@h3ndrk.de>
2020-12-18 16:55:08 +0100

    Implement model wrapper

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
+#include <string>
+#include <iostream>
 #include <boost/python.hpp>
 #include <boost/python/numpy.hpp>
+#include "CompiledNN.h"
+#include "Model.h"
 
 namespace py = boost::python;
 namespace np = py::numpy;
@@ -10,9 +14,44 @@ np::ndarray greet()
   // return "hello, world";
 }
 
-BOOST_PYTHON_MODULE(CompiledNN)
+struct ModelWrapper
 {
+  NeuralNetwork::Model model;
+
+  ModelWrapper() {}
+  ModelWrapper(const std::string &file) : model{file} {}
+
+  void setInputUInt8(std::size_t index)
+  {
+    model.setInputUInt8(index);
+  }
+
+  bool isInputUInt8(std::size_t index) const
+  {
+    return model.isInputUInt8(index);
+  }
+
+  void clear()
+  {
+    model.clear();
+  }
+
+  void load(const std::string &file)
+  {
+    model.load(file);
+  }
+};
+
+BOOST_PYTHON_MODULE(PyCompiledNN)
+{
+  NeuralNetwork::Model model;
   Py_Initialize();
   np::initialize();
   py::def("greet", greet);
+  py::class_<ModelWrapper, boost::noncopyable>("Model")
+      .def(py::init<std::string>())
+      .def("setInputUInt8", &ModelWrapper::setInputUInt8)
+      .def("isInputUInt8", &ModelWrapper::isInputUInt8)
+      .def("clear", &ModelWrapper::clear)
+      .def("load", &ModelWrapper::load);
 }
